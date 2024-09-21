54f80ae6666fb3fb1aafe126c2c0950a1de45c9d
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-21 16:41:26 -0300

    quote

---
+++ src/components/quote/index.jsx
@@ -0,0 +1,6 @@
+import styles from "./quote.module.css";
+import { Quotation } from "../../assets/images";
+
+export default function Quote() {
+  return <div className={styles.quoteContainer}></div>;
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,8 +4,8 @@ import styles from "./home.module.css";
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
-      <Aside />
       <Header />
+      <Aside />
       <Hero />
     </div>
   );
