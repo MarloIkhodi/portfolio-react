601212fa7609637ef7329e35985acf8234e7e572
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-22 20:59:20 -0300

    Aside on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Home, Work } from "./pages";
-import { Header } from "./components";
+import { Header, Aside } from "./components";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <Router>
       <Header />
+      <Aside />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
