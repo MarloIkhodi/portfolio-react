7d8b3d2530d8b2ef64b3012c5ee5b45568e3ec77
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-24 11:34:18 -0300

    wip quote

--- src/components/quote/index.jsx
+++ src/components/quote/index.jsx
@@ -5,12 +5,12 @@ export default function Quote() {
   return (
     <div className={styles.portfolioContainer}>
       <aside className={styles.asideContainer}>
-        <div>
+        <div className={styles.topSide}>
           <img src={Quotation} alt="Quotes" />
-          <p>Mass moves mass</p>
+          <p>Mass moves mass, Mass moves mass</p>
           <img src={Quotation} alt="Quotes" />
         </div>
-        <div>
+        <div className={styles.bottomSide}>
           <p>- Newtons' Law</p>
         </div>
       </aside>
