6cf4a5d5bd1857deaf6da8f0a9620c4cd26e7586
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-26 17:59:36 -0300

    resizing - quote

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <Hero />
-      {/* <Quote />
-      <AboutMe
+      <Quote />
+      {/* <AboutMe
         aboutInside
         button />
       <Projects /> */}
